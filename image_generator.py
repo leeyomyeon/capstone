7e8301dcc42f10725aad8e598c7276276ba891de
yyiz1135@gmail.com <44407364+yyiz1135@users.noreply.github.com>
2020-06-02 23:04:49 +0900

    1차 수정

--- image_generator.py
+++ image_generator.py
@@ -1,17 +1,18 @@
 import numpy as np
+import os
 
 np.random.seed(5)
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 data_aug_gen = ImageDataGenerator(rescale=1./255, rotation_range=15,width_shift_range=0.1, height_shift_range=0.1,shear_range=0.5,zoom_range=[0.8, 2.0],horizontal_flip=True,vertical_flip=True, fill_mode='nearest')
 
-img = load_img('test_set/1/1 (1154).JPEG')
-x = img_to_array(img)
-x = x.reshape((1,) + x.shape)
-
-i = 0
-
-for batch in data_aug_gen.flow(x, batch_size=1, save_to_dir='test_set/1_generating', save_prefix='tri', save_format='JPEG'):
-    i += 1
-    if i > 30: 
-        break 
+img_list = os.listdir('./generator')
+for filename in img_list:
+    img = load_img('./generator/'+filename)
+    x = img_to_array(img)
+    x = x.reshape((1,) + x.shape)
+    i = 0
+    for batch in data_aug_gen.flow(x, batch_size=1, save_to_dir='./generator/result', save_prefix='tri', save_format='JPEG'):
+        i += 1
+        if i > 30:
+            break
